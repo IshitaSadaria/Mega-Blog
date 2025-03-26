d4f01746dd52d637f0dbef39febae11e3c11cd6c
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-03-26 22:37:14 +0530

    Auth configuration

---
+++ src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();     // Create a new instance/object of the Client class, so that we can use its methods to authenticate the user.
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client);      
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call another method 
+                return this.login({email, password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+           return await this.account.deleteSession.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+           return await this.account.get(); 
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();      // Create a new instance/object of the AuthService class, so that we can use its methods to authenticate the user.
+
+export default AuthService;
