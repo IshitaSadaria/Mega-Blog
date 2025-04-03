53c8db4a05bf10ce1e4a8058193db7e02f9927d8
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-04-03 23:52:19 +0530

    importing all the components

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,10 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import SignUp from "./SignUp"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
 
 export { 
     Header, 
@@ -11,5 +15,9 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE 
+    RTE ,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard
 };
